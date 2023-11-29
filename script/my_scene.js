68144b69505a72c4c274c11b26e7f1856d4e0e67
itou <a.itou.sys22@morijyobi.ac.jp>
2023-11-29 15:07:18 +0900

    花子途中

--- script/my_scene.js
+++ script/my_scene.js
@@ -14,6 +14,7 @@ class MyScene extends Phaser.Scene {
         this.load.image('sky', 'assets/background.png');
         this.load.image('taro', 'assets/taro.png');
         this.load.image('jiro', 'assets/taro.png');
+        this.load.image('hanako', 'assets/hanako.png');
     }
 
     // シーン初期化処理
@@ -30,13 +31,16 @@ class MyScene extends Phaser.Scene {
         this.Text = this.add.text(600, 400, 'MyWorld', { fontSize: '28px', fill: '#FFF' ,fontFamily: "Arial"});
         this.a_Text = this.add.text(100, 50, '', { fontSize: '28px', fill: '#FFF' ,fontFamily: "Arial"});
         this.s_Text = this.add.text(100, 50, '', { fontSize: '28px', fill: '#FFF' ,fontFamily: "Arial"});
-  
+        let  randx = Phaser.Math.Between(50, 750) ; 
+        let randy =  Phaser.Math.Between(50, 200) ; 
+        this.w_hanako = this.add.image(randx, randy , 'hanako')
         ///WASDキーを検知できるようにする
         this.keys = {};
+        this.keys.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         this.keys.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
         this.keys.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
         this.keys.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-       
+
 
 
     }
@@ -106,6 +110,7 @@ class MyScene extends Phaser.Scene {
         }
         this.wasd_move(this.keys, this.a_Text);
         this.wasd_move(this.keys, this.s_Text);
+        this.wasd_move(this.keys, this.w_hanako);
         
     }
     wasd_move(keys, object){
@@ -116,8 +121,9 @@ class MyScene extends Phaser.Scene {
         }else if(keys.keyD.isDown){
             this.a_Text.setText('');
             this.s_Text.setText('');
+        }else if(keys.keyW.isDown){
+            this.w_hanak.setImage();
         }
 
-    }
-
+}
 }
